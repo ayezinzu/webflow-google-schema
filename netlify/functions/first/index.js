7aa07c6aaab1ea19b0a8f078e2ee54ec937e6a3d
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2022-02-19 17:26:25 +0530

    code refactor

--- netlify/functions/first/index.js
+++ netlify/functions/first/index.js
@@ -1,41 +1,43 @@
-
-const Axios = require('axios');
-const Webflow = require('webflow-api')
-let token = "3af01f7685f25901a74da7532f8052f0f07c2b50b9ced0d905d2689ab4847778"
+const Webflow = require("webflow-api");
+require("dotenv").config();
+let token = process.env.TOKEN;
 const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-  };
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
 
 exports.handler = async function (event) {
-    console.log(event.queryStringParameters)
-   
-    const webflow = new Webflow({ token: token });
+  console.log(event.queryStringParameters);
 
-    // Promise <Collection>
-    const item = await webflow.item({ collectionId: "6117a7b67f1352223a632286", itemId: event.queryStringParameters.item });
-    let includesQorA = (key) => {
-        if(key.includes("q") && key.length === 2){
-            return key
-        }
-    }
-    console.log(Object.keys(item).filter(includesQorA))
-    let filteredKeys = Object.keys(item).filter(includesQorA)
-    let faqArray = []
-    filteredKeys.forEach((filteredKey) => {
-        faqArray.push({
-            q: item[filteredKey],
-            a: item[`a${parseInt(filteredKey.slice(1))}`]
-        })
-    })
-    console.log(faqArray)
+  const webflow = new Webflow({ token: token });
 
-    return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({
-            message: faqArray
-        })
+  // Promise <Collection>
+  const item = await webflow.item({
+    collectionId: process.env.COLLECTION,
+    itemId: event.queryStringParameters.item,
+  });
+  let includesQorA = (key) => {
+    if (key.includes("q") && key.length === 2) {
+      return key;
     }
-}
+  };
+  console.log(Object.keys(item).filter(includesQorA));
+  let filteredKeys = Object.keys(item).filter(includesQorA);
+  let faqArray = [];
+  filteredKeys.forEach((filteredKey) => {
+    faqArray.push({
+      q: item[filteredKey],
+      a: item[`a${parseInt(filteredKey.slice(1))}`],
+    });
+  });
+  console.log(faqArray);
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({
+      message: faqArray,
+    }),
+  };
+};
